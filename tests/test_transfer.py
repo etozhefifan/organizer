5f87caa0b67196e73910c98d5eb69b69825b45e3
etozhefifan <coofi2012@gmail.com>
2023-02-24 16:29:29 +0300

    [Fixes] tests

--- tests/test_transfer.py
+++ tests/test_transfer.py
@@ -5,26 +5,37 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 root_dir_content = os.listdir(BASE_DIR)
 
 
+@pytest.fixture
+def 
+
 def test_check_folder():
     assert 'transfer' in root_dir_content
 
 
-class TestServerSocket():
+class TestServerSocket:
     from transfer.sockets.server_socket import ServerSocket
     server = ServerSocket()
+    port = int(os.getenv('PORT'))
+    host = os.getenv('HOST_SERVER')
 
     def test_server_socket_existence(self):
         assert self.server
 
     def test_server_port(self):
-        assert self.server.port == int(os.getenv('PORT'))
+        assert self.server.port == self.port
 
     def test_server_host(self):
-        assert self.server.host == os.getenv('HOST_SERVER')
+        assert self.server.host == self.host
 
     def test_server_separator(self):
         from transfer.config import SEPARATOR
         assert self.server.separator == SEPARATOR
 
     def test_setup_server_socket(self):
-        assert self.server.setup_socket()
+        self.server.setup_socket()
+        assert (
+            self.server.transfer_socket.getsockname() == (self.host, self.port)
+        )
+
+    # def test_server_accepts_connections(self):
+    #     assert self.server.transfer_socket.accept()
